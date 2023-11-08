51f0ffda3d660e649a74ba26168d9323b299cf67
Blade-Fusion <129384642+Blade-Fusion@users.noreply.github.com>
2023-11-08 15:09:31 +0100

    Made website

---
+++ 02-p5-css-intergration/sketch.js
@@ -0,0 +1,17 @@
+let currentPage = '#page1'
+function setup() {
+select ('#item1').mouseClicked( () => switchPage('#page1')) 
+select ('#item2').mouseClicked( () => switchPage('#page2'))
+select ('#item3').mouseClicked( () => switchPage('#page3'))
+select ('#item4').mouseClicked( () => switchPage('#page4'))
+}
+function switchPage(whichPage){
+  select(currentPage).removeClass('show')
+  select(whichPage).addClass('show')
+  currentPage = whichPage
+}
+
+function draw() {
+  background(220);
+}
+
